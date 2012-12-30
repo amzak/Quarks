c8d2299c420691262f958541487be23f05219e8f
oakio <ilya.dubadenko@gmail.com>
2012-12-30 22:36:55 +0300

    Added FormatWith string extention method

---
+++ Common.Tests/Extentions/StringExtTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Codestellation.Common.Extentions;
+using NUnit.Framework;
+
+namespace Codestellation.Common.Tests.Extentions
+{
+    [TestFixture]
+    public class StringExtTests
+    {
+        [Test]
+        public void Should_format_string_with()
+        {
+            const string template = "Hello, {0}!";
+            Assert.That(template.FormatWith("World"), Is.EqualTo("Hello, World!"));
+        }
+    }
+}
