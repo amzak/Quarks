3cdde4960ffbda953b797fcac1f728c41265ca23
oakio <ilya.dubadenko@gmail.com>
2012-12-30 23:28:02 +0300

    Added nuget.exe and EmptyIfNull extention method for Enumerable

---
+++ Common.Tests/Extentions/EnumerableExtTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Codestellation.Common.Extentions;
+using NUnit.Framework;
+
+namespace Codestellation.Common.Tests.Extentions
+{
+    [TestFixture]
+    public class EnumerableExtTests
+    {
+        [Test]
+        public void Should_return_original_if_original_is_not_null()
+        {
+            object[] self = {123, "Hello", DateTime.Now};
+            Assert.That(self.EmptyIfNull(), Is.EqualTo(self));
+        }
+
+        [Test]
+        public void Should_return_empty_if_original_is_null()
+        {
+            object[] self = null;
+            Assert.That(self.EmptyIfNull(), Is.EqualTo(Enumerable.Empty<object>()));
+        }
+    }
+}
---
+++ Common/Extentions/EnumerableExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codestellation.Common.Extentions
+{
+    public static class EnumerableExt
+    {
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> self)
+        {
+            return self ?? Enumerable.Empty<T>();
+        }
+    }
+}
