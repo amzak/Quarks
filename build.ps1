9c10c36c05ddf3beca01129d9421cd2aae42bcab
j.soldatenkov <yurysold@googlemail.com>
2013-01-14 23:53:39 +0400

    Implemented build with clean-up

--- build.ps1
+++ build.ps1
@@ -1,5 +1,14 @@
-﻿Task Default -depends Build
+﻿$invname = $MyInvocation.InvocationName
+write "Path to script $invname"
+$scriptPath = Split-Path $MyInvocation.InvocationName
 
-Task Build {
-   Exec { msbuild "solution.sln" }
+if($scriptPath)
+{
+    Set-Location $scriptPath    
 }
+
+$psakeModule = Get-ChildItem psake.psm1 -Path $scriptPath -Recurse
+
+Import-Module $psakeModule.FullName -force
+
+Invoke-psake -framework '4.0' -buildFile default.ps1
--- default.ps1
+++ default.ps1
@@ -1,6 +1,28 @@
-﻿$scriptPath = Split-Path $MyInvocation.InvocationName
-$psakeModule = Get-ChildItem psake.psm1 -Path $scriptPath -Recurse
+﻿Properties {
+    $basedir = Get-Location
+    $outputdir = '"' +  (Join-Path $basedir 'Build') + '"'
+    $framework = 4.0
+    $v4_net_version = (ls "$env:windir\Microsoft.NET\Framework64\v$framework*").Name
+    $msbuildpath = "$env:windir\Microsoft.NET\Framework\$v4_net_version\MsBuild.exe"
+}
 
-Import-Module $psakeModule.FullName -force
+Task Default -depends Build
 
-Invoke-psake -framework '4.0' -buildFile build.ps1  
+Task Build -depends Clean {
+    if(!$solution)
+    {
+        $solution = Get-Item -Path $basedir -Include *.sln
+    }
+
+    Exec { 
+       & $msbuildpath $solution "/p:OutDir=$outputdir"
+    }
+}
+
+Task Clean {
+    if(Test-Path $outputdir)
+    {
+        Remove-Item -Path $outputdir -Recurse -Force
+    }
+}
+
