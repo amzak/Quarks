915682d762df7c43bd2c7ad6b485b9e4aebe4971
Egor Gorbachev <egorbachev@corp.finam.ru>
2015-03-24 17:09:36 +0300

    #1: Added Some.Boolean method and test coverage

---
+++ src/Quarks.Tests/Testing/SomeBooleanTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Codestellation.Quarks.Testing;
+using NUnit.Framework;
+
+namespace Codestellation.Quarks.Tests.Testing
+{
+    [TestFixture]
+    public class SomeBooleanTests : SomeTests
+    {
+        [Test]
+        public void Should_generate_different_values()
+        {
+            // when
+            var results = ListOf(Some.Boolean);
+            var distinctResults = results.Distinct().ToList();
+
+            // then
+            Assert.That(distinctResults.Count, Is.Not.EqualTo(1), "All generated values are the same");
+        }
+
+        [Test]
+        public void Should_generate_uniform_distribution()
+        {
+            // given
+            const int listSize = 10000;
+
+            // when
+            var results = ListOf(Some.Boolean, listSize);
+            var stats = StatsFor(results);
+            var minFrequency = stats.MinFrequency;
+            var maxFrequency = stats.MaxFrequency;
+
+            // then
+            Assert.That(minFrequency, Is.Positive);
+            Assert.That(maxFrequency, Is.AtLeast(minFrequency));
+            Assert.That(maxFrequency / (double)minFrequency, Is.AtMost(1.1), "Frequency variation is too high");
+        }
+    }
+}
---
+++ src/Quarks/Testing/SomeBoolean.cs
@@ -0,0 +1,10 @@
+﻿namespace Codestellation.Quarks.Testing
+{
+    internal static partial class Some
+    {
+        public static bool Boolean()
+        {
+            return Int32(0, 1) == 1;
+        }
+    }
+}
