1e7b66c6fb6dd527684674604acc31782133a306
Egor Gorbachev <ggorbachev@corp.finam.ru>
2015-04-21 18:07:00 +0300

    Added Some.DateTime method and test coverage (issue #1)

---
+++ src/Quarks.Tests/Testing/SomeDateTimeTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Codestellation.Quarks.Testing;
+using NUnit.Framework;
+
+namespace Codestellation.Quarks.Tests.Testing
+{
+    [TestFixture]
+    public class SomeDateTimeTests : SomeTests
+    {
+        private static readonly object[] CommonCases =
+        {
+            new object[] { DateTime.MinValue, DateTime.MaxValue },
+            new object[] { new DateTime(2015, 1, 1), new DateTime(2016, 1, 1) },
+            new object[] { new DateTime(2015, 1, 1), new DateTime(2015, 2, 1) },
+            new object[] { new DateTime(2015, 1, 1), new DateTime(2015, 1, 2) },
+            new object[] { new DateTime(2015, 1, 1, 1, 1, 1), new DateTime(2015, 1, 1, 2, 1, 1) },
+            new object[] { new DateTime(2015, 1, 1, 1, 1, 1), new DateTime(2015, 1, 1, 1, 2, 1) },
+            new object[] { new DateTime(2015, 1, 1, 1, 1, 1), new DateTime(2015, 1, 1, 1, 1, 2) },
+        };
+
+        private static readonly object[] EqualBoundCases =
+        {
+            new object[] { DateTime.MinValue },
+            new object[] { DateTime.MaxValue },
+            new object[] { new DateTime(2015, 1, 1) },
+        };
+
+        [Test, TestCaseSource("CommonCases")]
+        public void Should_generate_different_values(DateTime min, DateTime max)
+        {
+            Should_generate_different_values(Some.DateTime, min, max);
+        }
+
+        [Test, TestCaseSource("CommonCases")]
+        public void Should_generate_uniform_distribution(DateTime min, DateTime max)
+        {
+            Should_generate_uniform_distribution(Some.DateTime, min, max, 2);
+        }
+
+        [Test, TestCaseSource("CommonCases")]
+        public void Should_generate_values_between_min_and_max(DateTime min, DateTime max)
+        {
+            Should_generate_values_between_min_and_max(Some.DateTime, min, max);
+        }
+
+        [Test, TestCaseSource("EqualBoundCases")]
+        public void Should_return_min_if_min_equal_to_max(DateTime bound)
+        {
+            Should_return_min_if_min_equal_to_max(Some.DateTime, bound);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Should_throw_if_min_greater_than_max()
+        {
+            // given
+            var min = DateTime.MinValue.AddTicks(1);
+            var max = DateTime.MinValue;
+
+            // when
+            Some.DateTime(min, max);
+        }
+    }
+}
---
+++ src/Quarks/Testing/SomeDateTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codestellation.Quarks.Testing
+{
+    internal static partial class Some
+    {
+        public static DateTime DateTime(DateTime min, DateTime max)
+        {
+            if (min > max)
+            {
+                throw new ArgumentException("Min value must be less or equal to max value");
+            }
+
+            if (min == max)
+            {
+                return min;
+            }
+
+            long binaryTime = Int64(min.Ticks, max.Ticks);
+            return System.DateTime.FromBinary(binaryTime);
+        }
+    }
+}
